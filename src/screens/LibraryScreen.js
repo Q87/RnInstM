4c700d9303c79d8bb492d18af252c2464c03f4ed
Qfab <Qfab.Shiz@fex.net>
2020-05-22 19:56:26 +0300

    Added receiving albums and photos from the local photo library

--- src/screens/LibraryScreen.js
+++ src/screens/LibraryScreen.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
+import CameraRoll from '@react-native-community/cameraroll';
 import {THEME} from '../theme';
 
 /**
  * Show media library screen
  */
 export const LibraryScreen = () => {
+  /**
+   * Get albums from a local photo library
+   */
+  const getAlbums = () => {
+    CameraRoll.getAlbums({
+      assetType: 'Photos',
+    })
+      .then(albums => {
+        console.log(albums);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+
+  /**
+   * Get photos from a local photo library
+   */
+  const getPhotos = () => {
+    CameraRoll.getPhotos({
+      first: 10,
+      assetType: 'Photos',
+    })
+      .then(photos => {
+        console.log(photos.edges);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+
   return (
     <View style={styles.wrapper}>
-      <Text>Library</Text>
+      <Text onPress={() => getAlbums()}>Get albums</Text>
+      <Text onPress={() => getPhotos()}>Get photos</Text>
     </View>
   );
 };
@@ -17,7 +50,7 @@ const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
     backgroundColor: THEME.MAIN_CONTENT_COLOR,
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center',
   },
 });
